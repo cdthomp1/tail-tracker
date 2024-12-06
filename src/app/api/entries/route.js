7093ffa9f92a6fcea35d698946f155a94e80f63b
cdthomp1 <camthomp96@gmail.com>
2024-12-06 16:17:52 -0700

    Remove log and sort by date

--- src/app/api/entries/route.js
+++ src/app/api/entries/route.js
@@ -3,14 +3,14 @@ import Entry from '../../models/Entry';
 
 export async function GET() {
     await connectToDatabase();
-    const entries = await Entry.find({});
+    const entries = await Entry.find({}).sort({ date: 1 }); // Sort by date in descending order (-1 for descending, 1 for ascending)
     return new Response(JSON.stringify(entries), { status: 200 });
 }
+
 
 export async function POST(req) {
     await connectToDatabase();
     const data = await req.json();
-    console.log(data)
     const entry = new Entry(data);
     await entry.save();
     return new Response(JSON.stringify(entry), { status: 201 });
